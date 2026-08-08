fff3f58c1fbf5f97efe272893b1ff457cde624bc
AnOwlist <anowlist.bf@gmail.com>
2026-08-08 10:36:26 +0900

    zsh: format command execution duration

--- home/cui/programs/zsh.nix
+++ home/cui/programs/zsh.nix
@@ -69,6 +69,22 @@ in
       repo_information() {
         echo "%F{${promptColors.directory}}''${vcs_info_msg_0_%%/.} %F{${promptColors.metadata}}$vcs_info_msg_1_`git_dirty` $vcs_info_msg_2_%f"
       }
+      cmd_exec_time() {
+        local stop=$(date +%s)
+        local start=''${cmd_timestamp:-$stop}
+        local elapsed=$((stop - start))
+        (( elapsed > 5 )) || return
+
+        local hours=$((elapsed / 3600))
+        local minutes=$(((elapsed % 3600) / 60))
+        local seconds=$((elapsed % 60))
+        local duration=""
+
+        (( hours > 0 )) && duration+="''${hours}h "
+        (( minutes > 0 )) && duration+="''${minutes}m "
+        (( seconds > 0 || elapsed < 60 )) && duration+="''${seconds}s"
+        echo "''${duration% }"
+      }
       refined_precmd() {
         setopt localoptions nopromptsubst
         vcs_info
