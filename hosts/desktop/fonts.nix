8609030affc24ad9989e9ff8466773b91bb2ea4e
AnOwlist <anowlist.bf@gmail.com>
2026-08-08 22:26:02 +0900

    fonts: add noto-fonts

--- hosts/desktop/fonts.nix
+++ hosts/desktop/fonts.nix
@@ -4,6 +4,7 @@
     enableDefaultPackages = false;
     packages =
       (with pkgs; [
+        noto-fonts
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
         noto-fonts-color-emoji
