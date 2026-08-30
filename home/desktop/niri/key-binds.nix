2b3b48182d3388196d5a924f5d3d4b7c9bbf1907
AnOwlist <anowlist.bf@gmail.com>
2026-08-31 04:08:10 +0900

    feat(niri): improve workspace movement bindings

--- home/desktop/niri/key-binds.nix
+++ home/desktop/niri/key-binds.nix
@@ -89,15 +89,42 @@ in
   "Mod+7".action.focus-workspace = 7;
   "Mod+8".action.focus-workspace = 8;
   "Mod+9".action.focus-workspace = 9;
-  "Mod+Ctrl+1".action.move-column-to-workspace = 1;
-  "Mod+Ctrl+2".action.move-column-to-workspace = 2;
-  "Mod+Ctrl+3".action.move-column-to-workspace = 3;
-  "Mod+Ctrl+4".action.move-column-to-workspace = 4;
-  "Mod+Ctrl+5".action.move-column-to-workspace = 5;
-  "Mod+Ctrl+6".action.move-column-to-workspace = 6;
-  "Mod+Ctrl+7".action.move-column-to-workspace = 7;
-  "Mod+Ctrl+8".action.move-column-to-workspace = 8;
-  "Mod+Ctrl+9".action.move-column-to-workspace = 9;
+  "Mod+Shift+1".action.move-column-to-workspace = [
+    { focus = false; }
+    1
+  ];
+  "Mod+Shift+2".action.move-column-to-workspace = [
+    { focus = false; }
+    2
+  ];
+  "Mod+Shift+3".action.move-column-to-workspace = [
+    { focus = false; }
+    3
+  ];
+  "Mod+Shift+4".action.move-column-to-workspace = [
+    { focus = false; }
+    4
+  ];
+  "Mod+Shift+5".action.move-column-to-workspace = [
+    { focus = false; }
+    5
+  ];
+  "Mod+Shift+6".action.move-column-to-workspace = [
+    { focus = false; }
+    6
+  ];
+  "Mod+Shift+7".action.move-column-to-workspace = [
+    { focus = false; }
+    7
+  ];
+  "Mod+Shift+8".action.move-column-to-workspace = [
+    { focus = false; }
+    8
+  ];
+  "Mod+Shift+9".action.move-column-to-workspace = [
+    { focus = false; }
+    9
+  ];
 
   "Mod+Alt+Left".action.set-window-width = "-10%";
   "Mod+Alt+Right".action.set-window-width = "+10%";
